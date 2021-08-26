26ff1332f01be7cb9e78979da6e971ddbcf60b46
bubu_lab <buwogg@gmail.com>
2021-08-26 21:08:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="(item) in users" :key="item.id">{{item.name}}</li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'App',
+  data(){
+    return{
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(res=>{
+      console.log(res.data)
+      this.users = res.data;
+    })
+  }
+}
+</script>
+
+<style>
+</style>
